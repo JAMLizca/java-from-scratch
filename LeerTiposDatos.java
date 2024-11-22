57d7f18c81a2dc5e9f758dd9d748b1127cabe8c6
JAMLizca <josemontenegroqc@gmail.com>
2024-11-21 21:18:57 -0500

    Basic java

--- LeerTiposDatos.java
+++ LeerTiposDatos.java
@@ -7,6 +7,25 @@ public class LeerTiposDatos {
         System.out.println("Ingresa tu edad =");
         var edad = consola.nextInt();
         System.out.println("edad = " + edad);
-        
+
+        // Crear el objeto Scanner para leer datos desde la consola
+        var nombre = new Scanner(System.in);
+
+         //En el trancurso del curso se explicara ese tema que son exepciones en java
+        try {
+            // Solicitar al usuario que ingrese su edad
+            System.out.print("Por favor, ingresa tu edad: ");
+            var anio = consola.nextInt(); // Leer un número entero
+            System.out.println("Tu edad es: " + anio);
+                } catch (java.util.InputMismatchException e) {
+                    // Manejar el caso en que el usuario no ingrese un número válido
+                    System.out.println("Error: Debes ingresar un número entero.");
+                } finally {
+                    // Cerrar el Scanner para liberar recursos
+                    consola.close();
+                }
+            }
+        }
+
     }
 }
