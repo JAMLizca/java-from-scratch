52146e5cc4ae0db96a61dd29b7d1ec5e0eaba7fc
JAMLizca <josemontenegroqc@gmail.com>
2024-11-22 20:42:52 -0500

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,23 @@
+package logica;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("***Operadores Aritmeticos***");
+        int a= 56, b= 2, resultado=0;
+        //suma
+        resultado= a+b;
+        System.out.println("resultado suma" +" "+ resultado);
+
+        //resta
+        resultado= a-b;
+        System.out.println("Resultado resta"+" "+resultado);
+
+        //multiplicacion
+        resultado= a*b;
+        System.out.println("Resultado multiplicacio"+ " "+ resultado);
+
+        //division
+        resultado= a/b;
+        System.out.println("Resultado division"+ " "+resultado);
+    }
+}
