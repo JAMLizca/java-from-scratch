fe6565feb51d9af701aeb57906940eb2d46272d9
JAMLizca <josemontenegroqc@gmail.com>
2024-11-22 20:03:16 -0500

    Basic java

--- GeneradorIde.java
+++ GeneradorIde.java
@@ -1,8 +1,22 @@
 package logica;
+import java.util.Random;
 import java.util.Scanner;
 public class GeneradorIde {
     public static void main(String[] args) {
         System.out.println("***Generador de Ide***");
+        var consola = new Scanner (System.in);
+        var random = new Random();
 
+        //solicitar nombre
+        System.out.println("Ingresa tu nombre");
+        var nombre = consola.nextLine();
+
+        //solicitar apellido
+        var apellido = consola.nextLine();
+
+        //solicitar año nacimiento
+        System.out.println("Cual es tu año de nacimiento");
+        var anio = consola.nextLine();
+        
     }
 }
