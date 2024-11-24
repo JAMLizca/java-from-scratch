b9d76a5248747ff416c931b509a46251a566de87
JAMLizca <josemontenegroqc@gmail.com>
2024-11-23 22:11:07 -0500

    Ejercicio tres de operadores logicos

---
+++ RangoVariable.java
@@ -0,0 +1,18 @@
+package logica;
+import java.util.Scanner;
+public class RangoVariable {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.println("Proporciona un dato entero");
+        var dato = Integer.parseInt(consola.nextLine());
+        //revisar si esta dentro del campo (entre 1 y 2)
+        var estRango = dato >=1 && dato <= 10;
+        System.out.println("variable entre el rango = " +" "+ estRango);
+
+        // revisar logica inversa
+        var estaFueraRango = !(dato >=1 &&  dato <= 10);
+        System.out.println("variable fuera de rango  = " +" "+ estaFueraRango);
+        
+
+    }
+}
