6a1460157ea7f27498337e041df3ca6826dc34c4
JAMLizca <josemontenegroqc@gmail.com>
2024-12-01 14:43:04 -0500

    Sentencia if

---
+++ SentenciaIf.java
@@ -0,0 +1,12 @@
+package logica;
+
+public class SentenciaIf {
+    public static void main(String[] args) {
+        //uso de la sentencia if
+        int edad= 30;
+        if (edad>=20){
+            System.out.println("Estas viejo men :V");
+        }
+        
+    }
+}
