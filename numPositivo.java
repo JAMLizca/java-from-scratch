bece90649e40b6132ec670e22ecdfeca53fc375e
JAMLizca <josemontenegroqc@gmail.com>
2024-12-02 21:01:06 -0500

    ejemplo sentencia if else if

--- numPositivo.java
+++ numPositivo.java
@@ -4,7 +4,7 @@ public class numPositivo {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
         System.out.println("Ingresa cualquier numero:");
-        int numEntero = consola.nextInt();
+        int numEntero =Integer.parseInt(consola.nextLine()) ;
         if (numEntero>0){
             System.out.println("Tu numero espositivo");
         }else if(numEntero<0){
