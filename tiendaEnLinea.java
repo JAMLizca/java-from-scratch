0c28d869732f30b571ee3580a39c3cbadc2fa27a
JAMLizca <josemontenegroqc@gmail.com>
2024-12-03 14:47:15 -0500

    Tienda en linea (ejemplo con if else if)

--- tiendaEnLinea.java
+++ tiendaEnLinea.java
@@ -16,7 +16,7 @@ public class tiendaEnLinea {
         var descuento = 0.0;
 
         if( totalCompra >=  MONTO_COMPRA && miembro){
-            descuento= 0.1; //
+            descuento= 0.1;
         }
         else if (miembro){
             descuento= 0.5; //descuento del 5%
@@ -24,19 +24,19 @@ public class tiendaEnLinea {
             descuento = 0; //descuento del 0%
         }
         //calculos respectivos para el monto final
-        if (descuento !=0){
+        if (descuento !=0) {
             var montoDescuento = totalCompra * descuento;
             var montoFinal = totalCompra - montoDescuento;
             System.out.printf("""
-                    %nFelicidades, has obtenido un descuento del %.=f%%
-                    Monto de la compra: $%.2f
-                    Monto del descuento: $%.2f
-                    Monto final de la compra con descuento: $%.2f\n
-                    """, descuento * 100, totalCompra, montoDescuento);
-        }
-        else{
+                            %nFelicidades, has obtenido un descuento del %.2f%%
+                            Monto de la compra: $%.2f
+                            Monto del descuento: $%.2f
+                            Monto final de la compra con descuento: $%.2f\n
+                            """, descuento * 100, totalCompra, montoDescuento, totalCompra - montoDescuento); // Calcula el monto final
+
+        }else{
             System.out.printf("""
-                    %NNo obtuvistes ningun descuento.
+                    %nNo obtuvistes ningun descuento.
                     Te invito hacer parte de la tienda
                     Monto final de la compra: $%.2f%n
                     """);
