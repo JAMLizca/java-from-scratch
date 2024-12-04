207e84153c737a6f0aa43950e6babd2d6e5412fe
JAMLizca <josemontenegroqc@gmail.com>
2024-12-04 14:39:11 -0500

    Sistema bancario(if y operador not, logica inversa

---
+++ sistemaBancario.java
@@ -0,0 +1,17 @@
+package logica;
+import java.util.Scanner;
+public class sistemaBancario {
+    public static void main(String[] args) {
+        System.out.println("*** Sistema Bancario ***");
+        var consola = new Scanner (System.in);
+        System.out.print("Desea salir del sistema (true/flasse)?");
+        var salirSistema = Boolean.parseBoolean(consola.nextLine());
+        //verificacion (aplicamos logica inversa)
+        if (!salirSistema){
+            System.out.println("Continuamos dentro del sistema.....");
+        }else{
+            System.out.println("Saliendo del siistema....");
+        }
+
+    }
+}
