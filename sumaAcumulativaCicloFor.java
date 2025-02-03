b0ba9118d1d73317ccad2bf44b97f346282c6d5f
JAMLizca <josemontenegroqc@gamil.com>
2025-02-03 08:54:31 -0500

    cumaIterativaConCicloWhile

---
+++ sumaAcumulativaCicloFor.java
@@ -0,0 +1,15 @@
+package logica;
+
+public class sumaAcumulativaCicloFor {
+    public static void main(String[] args) {
+        final var MAXIMO = 5;
+        var acumaldor =0;
+        //ieterar los valores
+       var num = 1;
+        while (num<= MAXIMO){
+            acumaldor +=num++;
+            System.out.println("la suma de los num"+ " "+ MAXIMO + " num ="+ " "+ acumaldor);
+
+        }
+    }
+}
