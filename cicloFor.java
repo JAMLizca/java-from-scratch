ab33b402993c24c496728a719d2a6300f9879b70
JAMLizca <josemontenegroqc@gamil.com>
2025-02-03 08:41:37 -0500

    ejercicio ciclo for

---
+++ cicloFor.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class cicloFor {
+    public static void main(String[] args) {
+        for (var i =1; i<5; i++){
+            System.out.println(i);
+        }
+
+        //otro ejercio
+        System.out.println("--Ciclo For--");
+        for(var letra = 1; letra <= 25; letra++ ){
+            System.out.println(letra);
+        }
+
+    }
+}
