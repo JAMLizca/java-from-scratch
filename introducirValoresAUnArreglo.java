672f5a3580d322d3ae5182808330bd034df01939
JAMLizca <josemontenegroqc@gamil.com>
2025-02-03 21:57:36 -0500

    introducir valores a un arreglo con java.util.Scanner

---
+++ introducirValoresAUnArreglo.java
@@ -0,0 +1,16 @@
+package logica;
+import java.util.Scanner;
+public class introducirValoresAUnArreglo {
+    public static void main(String[] args) {
+        var consola= new Scanner(System.in);
+        int[] arreglo = new int[5];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese un numero: ");
+            arreglo[i]= consola.nextInt();
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("El valor del arreglo ["+i+"] es: "+arreglo[i]);
+        }
+
+    }
+}
