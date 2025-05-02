29162cdbba7923ecc42ebb75fa2f5a4721ace081
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-01 21:15:33 -0500

    Update Argumentos_Variables

--- Tema_Avanzados/Argumentos_Variables/ArgumentosVariables.java
+++ Tema_Avanzados/Argumentos_Variables/ArgumentosVariables.java
@@ -2,14 +2,20 @@ package logica.Tema_Avanzados.Argumentos_Variables;
 
 public class ArgumentosVariables {
     public static void main(String[] args) {
-        imprimirNumeros(1,2,3,4,5); //argumentos variables varargas
-
-
+        // imprimirNumeros(1,2,3,4,5); //argumentos variables varargas
+        variosParametros("yo",12,22,22);
     }
+
+    static void variosParametros(String nombre, int edad.. numeros) {
+    }
+
     //Metodo
-//     static void imprimirNumeros(int i, int i1, int i2, int i3, int i4) {
+//static void imprimirNumeros(int i, int i1, int i2, int i3, int i4) {
     //lo que hace el int... numeros es que recibe un numero variable de argumentos, crea un tipo de arreglo
     static void imprimirNumeros(int... numeros) {
+        for (var i=0; i<numeros.length; i++){
+            System.out.print(numeros[i]+" ");
+        }
 
     }
 
