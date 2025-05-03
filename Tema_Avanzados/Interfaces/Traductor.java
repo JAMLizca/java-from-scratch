1359a8ed693ca7574c2d5eb98c7b18ca3f19a8af
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-02 22:23:07 -0500

    Update interfaz

--- Tema_Avanzados/Interfaces/Traductor.java
+++ Tema_Avanzados/Interfaces/Traductor.java
@@ -1,27 +1,44 @@
 package logica.Tema_Avanzados.Interfaces;
 
 public interface Traductor {
-   // publicos y abstractos
+    // publicos y abstractos
     void traducir();
-    //metodos por implementacion por default
-    default void iniciarTraductor(){
+
+    // métodos con implementación por default
+    default void iniciarTraductor() {
         System.out.println("Traductor iniciado");
     }
 }
 
-//clases que implementas la interfaz
-class Ingles implements Traductor{
+// clases que implementan la interfaz
+class Ingles implements Traductor {
     @Override
     public void traducir() {
-        System.out.println("Traducir a ingles");
+        System.out.println("Traducir a inglés");
     }
 }
-//clase de prueba y polimorfismo
-class PruebaTraductor{
+
+class Frances implements Traductor {
+    @Override
+    public void traducir() {
+        System.out.println("Traducir a francés");
+    }
+
+    @Override
+    public void iniciarTraductor() {
+        System.out.println("Traductor iniciado a francés");
+    }
+}
+
+// clase de prueba y polimorfismo
+class PruebaTraductor {
     public static void main(String[] args) {
         Traductor ingles = new Ingles();
         ingles.traducir();
         ingles.iniciarTraductor();
+
+        Traductor frances = new Frances();
+        frances.traducir();
+        frances.iniciarTraductor();
     }
 }
-
