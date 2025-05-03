7f5efb70381481ee5276e16af013a3073a06ce0c
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-02 21:56:20 -0500

    Update clases abstractas

--- Tema_Avanzados/Clases_Abstractas/Clase_Abstracta.java
+++ Tema_Avanzados/Clases_Abstractas/Clase_Abstracta.java
@@ -2,21 +2,25 @@ package logica.Tema_Avanzados.Clases_Abstractas;
 
 public class Clase_Abstracta {
     public static void main(String[] args) {
-        //Creacion objeto de esta clase
-        //FiguraGeometrica figuraGeometrica = new FiguraGeometrica() {} nos va a producir error porque no se puedde isntanciar
+        // Creacion objeto de esta clase
+        // FiguraGeometrica figuraGeometrica = new FiguraGeometrica() {} nos va a producir error porque no se puede instanciar
+        // Podemos crear un objeto de la clase hija y a la vez aplicamos polimorfismo
+        FiguraGeometrica figuraGeometrica = new Rectangulo(); // Aplicamos polimorfismo
+        figuraGeometrica.dibujar();
+    }
 
+    // Clase abstracta
+    // No podemos crear objetos de esta clase
+    abstract static class FiguraGeometrica {
+        public abstract void dibujar();
+    }
 
-        //clase abstracta
-        //No podemos crear objetos de esta clase
-        abstract class FiguraGeometrica {
-            public abstract void dibujar();
-        }
-        //clase hija que hereda de figura geometrica
-        class Rectangulo extends FiguraGeometrica {
-            //sobrescritura de metodo
-            @Override
-            public void dibujar() {
-                System.out.println("Dibujar rectangulo");
-            }
+    // Clase hija que hereda de FiguraGeometrica
+    static class Rectangulo extends FiguraGeometrica {
+        // Sobrescritura de método
+        @Override
+        public void dibujar() {
+            System.out.println("Dibujar rectángulo");
         }
     }
+}
