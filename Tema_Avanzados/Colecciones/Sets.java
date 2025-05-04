cbb7f3326b555d72c29f10ae486cd8c51bc5854b
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-04 11:44:57 -0500

    Set List

---
+++ Tema_Avanzados/Colecciones/Sets.java
@@ -0,0 +1,11 @@
+package logica.Tema_Avanzados.Colecciones;
+
+import java.util.TreeSet;
+import java.util.Set;
+
+public class Sets {
+    public static void main(String[] args) {
+        Set<String> conjunto = new TreeSet<>();
+        
+    }
+}
