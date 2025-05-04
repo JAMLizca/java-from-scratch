9472ea8cae74037154e653d0fc2d1f6078e98163
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-04 11:47:05 -0500

    Update

--- Tema_Avanzados/Colecciones/Sets.java
+++ Tema_Avanzados/Colecciones/Sets.java
@@ -4,8 +4,13 @@ import java.util.TreeSet;
 import java.util.Set;
 
 public class Sets {
+    //con set no se pueden repetir elementos
     public static void main(String[] args) {
         Set<String> conjunto = new TreeSet<>();
-        
+        conjunto.add("Hola");
+        conjunto.add("Mundo");
+        conjunto.add("Mundo");
+        System.out.println(conjunto);
+        conjunto.forEach(System.out::println);
     }
 }
