29c9749fe58f9f71e11807b3f9d8ad6e6845d80e
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-04 11:52:42 -0500

    Update Map

--- Tema_Avanzados/Colecciones/Mapa/Mapa.java
+++ Tema_Avanzados/Colecciones/Mapa/Mapa.java
@@ -1,7 +1,15 @@
 package logica.Tema_Avanzados.Colecciones.Mapa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapa {
     public static void main(String[] args) {
-        
+        Map<String, String> persona = new HashMap<>();
+        persona.put("nombre", "Juan");
+        persona.put("apellido", "Perez");
+        persona.put("edad", "20");
+        System.out.println(persona);
+        persona.forEach((k,v) -> System.out.println(k + " = " + v));
     }
 }
