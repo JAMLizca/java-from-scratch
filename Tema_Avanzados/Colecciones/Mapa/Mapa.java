77b10eed165ede87726ca7be809d4a1f9bb0ea8d
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-04 11:54:14 -0500

    Update map

--- Tema_Avanzados/Colecciones/Mapa/Mapa.java
+++ Tema_Avanzados/Colecciones/Mapa/Mapa.java
@@ -5,11 +5,16 @@ import java.util.Map;
 
 public class Mapa {
     public static void main(String[] args) {
+        //Mapas o diccionarios
         Map<String, String> persona = new HashMap<>();
         persona.put("nombre", "Juan");
         persona.put("apellido", "Perez");
         persona.put("edad", "20");
+        //Forma uno
+        persona.entrySet().forEach(System.out::println);
+        //forma dos
         System.out.println(persona);
+        //forma tres
         persona.forEach((k,v) -> System.out.println(k + " = " + v));
     }
 }
