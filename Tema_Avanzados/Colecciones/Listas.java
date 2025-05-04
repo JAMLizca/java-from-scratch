6db369331872ab5a5d5fb00bf06e039dc21577d7
Alejandro Montenegro <u20241222690@usco.edu.co>
2025-05-04 11:27:41 -0500

    Update listas

--- Tema_Avanzados/Colecciones/Listas.java
+++ Tema_Avanzados/Colecciones/Listas.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 public class Listas{
     public static void main(String[] args) {
-        List miLista = new ArrayList();
+        //List miLista = new ArrayList();
+        //Manejo de datos generics
+        List<String> miLista = new ArrayList<>();
         miLista.add("Lunes");
         miLista.add("Martes");
         miLista.add("Miercoles");
@@ -13,9 +15,13 @@ public class Listas{
         miLista.add("Viernes");
         miLista.add("Sabado");
         miLista.add("Domingo");
+
         for (var dia:miLista){
             System.out.println(">" + dia + "<");
         }
+
+        //Funciones lamda (funcion anonima de un codigo muy compacto) programacion funcional
+        miLista.forEach(dia -> System.out.println(">" + dia + "<"));
 
     }
 }
